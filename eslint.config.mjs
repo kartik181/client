b6a909bf34bbc3bfe3bf8c1a9daa5f8712717954
kartik181 <kartiksethi6798@gmail.com>
2025-03-14 05:08:33 +0530

    setup project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,14 +3,21 @@ import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+const eslintConfig = {
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript', 'prettier', 'plugin:tailwindcss/recommend'],
+    plugins: ['prettier'],
+    rules:{
+      'prettier/prettier': 'error',
+      'react/no-escape-entities': 'off',
+    },
+  }),
+};
 
 export default eslintConfig;
