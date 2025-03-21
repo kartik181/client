10d39bb6208995b6bc31cc65d13281af305a7b88
kartik181 <kartiksethi6798@gmail.com>
2025-03-21 13:23:12 +0530

    clerkMiddareware

--- src/components/summaries/summary-header.tsx
+++ src/components/summaries/summary-header.tsx
@@ -47,7 +47,7 @@ export function SummaryHeader({
           <Button
             size="sm"
             variant="link"
-            className="group flex items-center gap-1 sm:gap-2 hover:bg-white/80 backdrop-blur-xs rounded-full transition-all duration-200 shadow-xs hover:shadow-md border border-rose-100/30  bg-rose-100 px-2 sm:px-3"
+            className=" hover:no-underline  group flex items-center gap-1 sm:gap-2 hover:bg-white/80 backdrop-blur-xs rounded-full transition-all duration-200 shadow-xs hover:shadow-md border border-rose-100/30  bg-rose-100 px-2 sm:px-3"
           >
             <ChevronLeft className="h-3 w-3 sm:h-4 sm:w-4 text-rose-500 transition-transform group hover:translate-x-0.5" />
             <span className="text-xs sm:text-sm text-muted-foreground font-medium">
